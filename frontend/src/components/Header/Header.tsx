4ada5e50601c730964e38e53dd6faa59bb35e585
Will Nilges <wdn5796@rit.edu>
2024-01-11 15:27:52 -0700

    Change up logo and a few wordings

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -7,13 +7,9 @@ import { makeClass } from '/src/utils'
 
 import styles from './Header.module.scss'
 
-const samuraiBob = localFont({
-  src: './samuraibob.woff2',
-  fallback: ['sans-serif'],
-})
-const molot = localFont({
-  src: './molot.woff2',
-  fallback: ['sans-serif'],
+const roboto = localFont({
+  src: './roboto.woff2',
+  fallback: ['monospace'],
 })
 
 interface HeaderProps {
@@ -28,11 +24,11 @@ const Header = async ({ isFull, isSmall }: HeaderProps) => {
   return <header className={styles.header} data-small={isSmall}>
     {isFull ? <>
       {!isSmall && <img className={styles.bigLogo} src={logo.src} height={512} width={512} alt="" />}
-      <span className={makeClass(styles.subtitle, 'sans-serif', !/^[A-Za-z ]+$/.test(t('home:create')) && styles.hasAltChars)}>{t('home:create')}</span>
+      <span className={makeClass(styles.subtitle, roboto.className, !/^[A-Za-z ]+$/.test(t('home:create')) && styles.hasAltChars)}>{t('home:create')}</span>
     </> : <Link href="/" className={styles.link}>
       <div className={styles.top}>
         <img className={styles.logo} src={logo.src} height={512} width={512} alt="" />
-        <span className={makeClass(styles.title, molot.className)}>MESH FIT</span>
+        <span className={makeClass(styles.subtitle, roboto.className)}>Schedule Service</span>
       </div>
     </Link>}
   </header>
